31e861d406bea2278d01e60d0f2956f7c6683fe1
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-11-20 16:04:40 +0000

    [Update]4-22

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  root to: "homes#top"
+
+
+  devise_for :users
+
+
+  root to: 'homes#top'
+  get "/homes/about" => "homes#about", as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+
+
+
+  #get 'homes/about', to: 'homes#about', as: 'about'
+ # get 'homes/top', to: 'homes#top', as: 'top'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
